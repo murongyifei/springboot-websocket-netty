ad12940bb67de231597ac2973adb5b115e96d7d5
wanghaiming <252127886@qq.com>
2024-07-12 14:46:36 +0800

    用@RequiredArgsConstructor代替@Autowired 注意遇到过问题!!! @RequiredArgsConstructor !!!会形成循环依赖 !!!慎用!!!

--- src/main/java/space/whm/websocket/controller/SendMsgController.java
+++ src/main/java/space/whm/websocket/controller/SendMsgController.java
@@ -4,13 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import space.whm.websocket.service.PushMsg2Service;
 import space.whm.websocket.service.PushMsgService;
 
+/**
+ * 用@RequiredArgsConstructor代替@Autowired
+ * 注意遇到过问题!!! @RequiredArgsConstructor !!!会形成循环依赖 !!!慎用!!!
+ */
 @RestController
 @RequiredArgsConstructor
 public class SendMsgController {
 
     final PushMsgService pushMsgService;
+
+    final PushMsg2Service pushMsg2Service;
 
     @GetMapping("/sendToUid")
     public String sendToUid(@RequestParam String uid) {
@@ -20,7 +27,7 @@ public class SendMsgController {
 
     @GetMapping("/sendAll")
     public String sendAll() {
-        pushMsgService.pushMsgToAll("Hello Everyone");
+        pushMsg2Service.pushMsgToAll("Hello Everyone");
         return "OK";
     }
 }
---
+++ src/main/java/space/whm/websocket/service/impl/PushMsg2ServiceImpl.java
@@ -0,0 +1,31 @@
+package space.whm.websocket.service.impl;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.springframework.stereotype.Service;
+import space.whm.websocket.config.NettyConfig;
+import space.whm.websocket.service.PushMsg2Service;
+
+import java.util.Objects;
+
+/**
+ * 推送消息接口实现类
+ */
+@Service
+public class PushMsg2ServiceImpl implements PushMsg2Service {
+
+    @Override
+    public void pushMsgToOne(String userId, String msg) {
+        Channel channel = NettyConfig.getChannel(userId);
+        if (Objects.isNull(channel)) {
+            throw new RuntimeException("未连接socket服务器");
+        }
+
+        channel.writeAndFlush(new TextWebSocketFrame(msg));
+    }
+
+    @Override
+    public void pushMsgToAll(String msg) {
+        NettyConfig.getChannelGroup().writeAndFlush(new TextWebSocketFrame(msg));
+    }
+}
